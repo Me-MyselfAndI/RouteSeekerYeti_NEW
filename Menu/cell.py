5c01cd15b12df7ce068aa436a6adc0bb0c48b897
iBoyGrisha() <32421843+Me-MyselfAndI@users.noreply.github.com>
2020-11-18 15:22:47 -0500

    Initial Commit

---
+++ Menu/cell.py
@@ -0,0 +1,45 @@
+import pygame
+
+WHITE = (255, 255, 255)
+YETI_BLUE = (150, 150, 255)
+YETI_BLUE_LIT = (180, 180, 255)
+RED_LIT = (200, 50, 50)
+RED = (170, 70, 70)
+GREEN_LIT = (80, 160, 80)
+GREEN = (80, 120, 80)
+YELLOW = (100, 100, 50)
+BLACK = (10, 10, 10)
+
+class Cell:
+    def __init__ (self, x, y, width, height, type = 'c'):
+        self.x, self.y, self.type = x, y, type
+        self.width, self.height = width, height
+
+        self.lit = False
+        self.positions = []
+        self.DEFAULT_TYPE = self.type
+
+    def display (self, win):
+        if self.type == 'c':
+            color = YETI_BLUE_LIT if self.lit else YETI_BLUE
+        elif self.type == 'w':
+            color = BLACK
+        elif self.type == 'l':
+            color = RED_LIT if self.lit else RED
+        elif self.type == 's':
+            color = GREEN_LIT if self.lit else GREEN
+        else:
+            print ("\t!!ALERT!!\nWrong definition of a cell:", self.x, self.y)
+            color = YELLOW
+        pygame.draw.rect(win, color, (self.x, self.y, self.width, self.height))
+        if (len(self.positions) != 0):
+            font = pygame.font.SysFont ('Arial', 9)
+            text = ''
+            for curr_num in self.positions:
+                text += str(curr_num) + ','
+            text = font.render(text, True, (0, 0, 0))
+            win.blit (text, (self.x, self.y))
+
+    def reset (self):
+        self.type = self.DEFAULT_TYPE
+        self.positions.pop ()
---
+++ Menu/menu.py
@@ -0,0 +1,213 @@
+import pygame
+from Menu.cell import Cell
+from Menu.button import Button
+from Menu.number_box import NumberBox
+
+win = pygame.display.set_mode((1700, 880))
+pygame.init()
+
+GREEN = (80, 120, 80)
+RED = (170, 70, 70)
+BLACK = (10, 10, 10)
+
+WHITE = (255, 255, 255)
+win.fill (WHITE)
+
+
+cells_amount = (108, 53)
+cell_width = 14
+cell_height = 11
+cells_offset = [45, 20, 1]  # x-offset; y-offset; offset btw cells
+
+
+cells = []
+for i in range (cells_amount[0]):
+    cells.append([])
+    for j in range (cells_amount[1]):
+        cells[i].append(Cell(cells_offset[0] + (cells_offset[2]+cell_width)*i, cells_offset[1] + (cells_offset[2]+cell_height)*j, cell_width, cell_height, 'c'))
+
+allie_sequence = []
+
+def set_point (cell_index_x, cell_index_y):
+    cells[cell_index_x][cell_index_y].positions.append(len(allie_sequence) + 1)
+    allie_sequence.append ([cell_index_y, cell_index_x])    # Reverse order because the main program has the map vertically,
+                                                            # and here it is horizontal
+    last_cell_coords = allie_sequence[len(allie_sequence) - 1]
+    cells[last_cell_coords[1]][last_cell_coords[0]].type = 'c'
+
+def set_loading ():
+    if len(allie_sequence) > 0:
+        last_cell_coords = allie_sequence[len(allie_sequence) - 1]
+        cells[last_cell_coords[1]][last_cell_coords[0]].type = 'l'
+        allie_sequence[len(allie_sequence) - 1].append ('l')      # load
+
+def set_deploying ():
+    if len(allie_sequence) > 0:
+        last_cell_coords = allie_sequence[len(allie_sequence) - 1]
+        cells[last_cell_coords[1]][last_cell_coords[0]].type = 's'
+        allie_sequence[len(allie_sequence) - 1].append ('s')      # shoot out the game piece
+
+def delete_last ():
+    if len(allie_sequence) > 0:
+        last_cell = allie_sequence.pop()
+        cells[last_cell[1]][last_cell[0]].reset()
+
+set_loading_button = Button (245, 800, 255, 40, 'Set a loading place in the last path point', set_loading)
+set_deploying_button = Button (530, 800, 285, 40, 'Set a deployment place in the last path point', set_deploying)
+deletion_button = Button (840, 800, 175, 40, 'Delete the last path point', delete_last)
+finish_button = Button (1045, 800, 100, 40, 'Done', False)
+
+mode = 0    # 0 - nothing happens; 1 - selection of path; 2 - selection of loading points; 3 - selection of shooting points
+
+lit_cells = []
+
+pygame.draw.rect(win, BLACK, (cells_offset[0] + 35, (cells_offset[2] + cell_height) * cells_amount[1] + 25, 250, 80))
+font = pygame.font.SysFont('Arial', 20)
+text = font.render('Game piece deployment points', True, GREEN)
+win.blit(text, (cells_offset[0] + 40, (cells_offset[2] + cell_height) * cells_amount[1] + 30))
+text = font.render('Game piece loading points', True, RED)
+win.blit(text, (cells_offset[0] + 40, (cells_offset[2] + cell_height) * cells_amount[1] + 60))
+
+font = pygame.font.SysFont('Arial', 13)
+
+team_number_box = NumberBox ('     Team Number', 400, 660, win, underscore_included=True)
+speed_box = NumberBox ('Average Speed During', 600, 660, win, title_line2='Autonomous ONLY')
+loading_time_box = NumberBox ('Average Loading Time', 800, 660, win)
+deployment_time_box = NumberBox ('Average Deployment Time', 1000, 660, win)
+
+done = False
+interrupted = False
+while not (done or interrupted):
+    for event in pygame.event.get():
+        # ------------------ Checks if the window was closed -------------------#
+        if event.type == pygame.QUIT:
+            interrupted = True
+        # ------------------------------ Over --------------------------------- #
+        #--------------- Checks if one of the cells was clicked ----------------#
+        mouse_x = pygame.mouse.get_pos()[0]
+        mouse_y = pygame.mouse.get_pos()[1]
+
+        within_x_range = 0 < mouse_x - cells_offset[0] <= (cells_offset[2] + cell_width) * cells_amount[0] - cells_offset[2]
+        within_y_range = 0 < mouse_y - cells_offset[1] <= (cells_offset[2] + cell_height) * cells_amount[1] - cells_offset[2]
+
+        # Put down lit cells:
+        for i in range (len(lit_cells)):
+            lit_cells[i].lit = False
+
+        if within_x_range and within_y_range:
+            within_x_range = 0 < mouse_x - cells_offset[0] <= (cells_offset[2] + cell_width) * cells_amount[0] - cells_offset[2]
+            within_y_range = 0 < mouse_y - cells_offset[1] <= (cells_offset[2] + cell_height) * cells_amount[1] - cells_offset[2]
+
+            cell_index_x = int((mouse_x - cells_offset[0]) / (cell_width + cells_offset[2]))
+            cell_index_y = int((mouse_y - cells_offset[1]) / (cell_height + cells_offset[2]))
+
+            cells[cell_index_x][cell_index_y].lit = True
+            if (not cells[cell_index_x][cell_index_y] in lit_cells):
+                lit_cells.append(cells[cell_index_x][cell_index_y])
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                set_point(cell_index_x, cell_index_y)
+                print ("Within the board:", str([cell_index_x, cell_index_y]))
+
+        # ------------------------------ Over --------------------------------- #
+
+        # ------ Checks if the set_loading_button was hovered over or clicked -----#
+        within_x_range = 0 < mouse_x - set_loading_button.x <= set_loading_button.width
+        within_y_range = 0 < mouse_y - set_loading_button.y <= set_loading_button.height
+
+        if within_x_range and within_y_range:
+            set_loading_button.display_active_mode(win)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                set_loading_button.display_clicked_mode(win)
+                set_loading_button.execute_function ()
+        else:
+            set_loading_button.display_inactive_mode(win)
+        # ------------------------------ Over --------------------------------- #
+
+        # --- Checks if the set_deploying_button was hovered over or clicked ---#
+        within_x_range = 0 < mouse_x - set_deploying_button.x <= set_deploying_button.width
+        within_y_range = 0 < mouse_y - set_deploying_button.y <= set_deploying_button.height
+
+        if within_x_range and within_y_range:
+            set_deploying_button.display_active_mode(win)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                set_deploying_button.display_clicked_mode(win)
+                set_deploying_button.execute_function()
+        else:
+            set_deploying_button.display_inactive_mode(win)
+        # ------------------------------ Over --------------------------------- #
+
+        # --- Checks if the set_deploying_button was hovered over or clicked ---#
+        within_x_range = 0 < mouse_x - deletion_button.x <= deletion_button.width
+        within_y_range = 0 < mouse_y - deletion_button.y <= deletion_button.height
+
+        if within_x_range and within_y_range:
+            deletion_button.display_active_mode(win)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                deletion_button.display_clicked_mode(win)
+                deletion_button.execute_function()
+        else:
+            deletion_button.display_inactive_mode(win)
+        # ------------------------------ Over --------------------------------- #
+
+        # --- Checks if the set_deploying_button was hovered over or clicked ---#
+        within_x_range = 0 < mouse_x - finish_button.x <= finish_button.width
+        within_y_range = 0 < mouse_y - finish_button.y <= finish_button.height
+
+        if within_x_range and within_y_range:
+            finish_button.display_active_mode(win)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                finish_button.display_clicked_mode(win)
+                done = True
+        else:
+            finish_button.display_inactive_mode(win)
+        # ------------------------------ Over --------------------------------- #
+
+        team_number_box.display(mouse_x, mouse_y, event)
+        speed_box.display(mouse_x, mouse_y, event)
+        loading_time_box.display(mouse_x, mouse_y, event)
+        deployment_time_box.display(mouse_x, mouse_y, event)
+
+    else:       # This will execute if the loop finished without a break
+        for i in range(cells_amount[0]):
+            if i % 5 == 0:
+                text = font.render(str (i/2), True, (0, 0, 0))
+                win.blit(text, (cells[i][j].x - 1, cells_offset[1] - 14))
+            for j in range(cells_amount[1]):
+                cells[i][j].display(win)
+                if i % 5 == 0:
+                    text = font.render(str(j/2), True, (0, 0, 0))
+                    win.blit(text, (cells_offset[0] - 25, cells[i][j].y - 2))
+
+        pygame.display.update ()
+
+
+        continue   # This way, if the for-loop finished without a break, we will do everything that needs to be done. If it
+                # Finishes by being broken out of, this branch will be ignored, and the break statement will be
+            # reached. So, pygame.quit() thus breaks both loops.
+        # Otherwise, if pygame.quit() is not activated, the continue will force python to skip the break.
+    break
+
+pygame.quit()
+
+if done:
+    print(allie_sequence)
+    team_number = team_number_box.text
+    team_speed = speed_box.text
+    team_l_time = loading_time_box.text
+    team_d_time = deployment_time_box.text
+
+    file = open('Allies_strategies/' + team_number + '.txt', 'a')
+
+    file.write(team_number + ' ')
+    file.write(team_speed + ' ')
+    file.write(team_l_time + ' ')
+    file.write(team_d_time + '\n')
+
+    for curr_coords in allie_sequence:
+        file.write('(' + str(curr_coords[0]) + ',' + str(curr_coords[1]) + ')' +(curr_coords[2] if len(curr_coords) > 2 else '') + '\n')
+
+    file.close()
+
+else:
+    print('\u001b[31m!!ALERT!!\nFinished by closing window:\n\tno file created, try again')
